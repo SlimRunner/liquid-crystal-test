1c2e05150b473e49916b330a233f5773c75f11a9
SlimRunner <dv.polygon@gmail.com>
2023-07-24 21:08:22 -0700

    code cleanup

--- Dialog.hpp
+++ Dialog.hpp
@@ -8,7 +8,7 @@ class Dialog {
 private:
   LiquidCrystal &m_screen;
   const int M_WIDTH;
-  const int M_HIGHT;
+  const int M_HEIGHT;
   int m_cadence;
   int m_flipdelay;
   int m_outdelay;
@@ -27,14 +27,15 @@ public:
   // default destructor
 
   void write(char message[]) {
-    const int SCR_SIZE = M_WIDTH * M_HIGHT;
+    const int SCR_SIZE = M_WIDTH * M_HEIGHT;
     // const int MSG_SIZE = sizeof(message) / sizeof(message[0]) - 1;
-    int iText, iScr, x, y, page, prevPage;
+    int iMsg, iScr, x, y, page, prevPage;
     prevPage = 0;
-    for (iText = 0; message[iText] != 0; ++iText) {
-      iScr = iText % SCR_SIZE;
+    for (iMsg = 0; message[iMsg] != 0; ++iMsg) {
+      iScr = iMsg % SCR_SIZE;
       x = iScr % M_WIDTH;
       y = iScr / M_WIDTH;
+      page = iMsg / SCR_SIZE;
       page = iText / SCR_SIZE;
       if (page == prevPage) {
         delay(m_cadence);
@@ -43,7 +44,7 @@ public:
         m_screen.clear();
       }
       m_screen.setCursor(x, y);
-      m_screen.print(message[iText]);
+      m_screen.write(message[iMsg]);
       prevPage = page;
     }
     delay(m_outdelay);
@@ -52,17 +53,17 @@ public:
 };
 
 inline Dialog::Dialog(LiquidCrystal &screen, int width, int height)
-    : m_screen(screen), M_WIDTH(width), M_HIGHT(height), m_cadence(60),
+    : m_screen(screen), M_WIDTH(width), M_HEIGHT(height), m_cadence(60),
       m_flipdelay(2000), m_outdelay(2000) {}
 
 Dialog::Dialog(LiquidCrystal &screen, int width, int height, int typingCadence,
                int pagingDelay)
-    : m_screen(screen), M_WIDTH(width), M_HIGHT(height),
+    : m_screen(screen), M_WIDTH(width), M_HEIGHT(height),
       m_cadence(typingCadence), m_flipdelay(pagingDelay),
       m_outdelay(pagingDelay) {}
 
 Dialog::Dialog(LiquidCrystal &screen, int width, int height, int typingCadence,
                int pagingDelay, int endingDelay)
-    : m_screen(screen), M_WIDTH(width), M_HIGHT(height),
+    : m_screen(screen), M_WIDTH(width), M_HEIGHT(height),
       m_cadence(typingCadence), m_flipdelay(pagingDelay),
       m_outdelay(endingDelay) {}
